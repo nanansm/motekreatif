9d299b525135e6ee257d15631fb33cf7df86f711
nanansm <76173154+nanansm@users.noreply.github.com>
2024-10-11 21:06:59 +0700

    Initial commit

---
+++ src/components/home-one/hero/HeroContent.jsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+import ShapeImg from "../../../assets/images/v1/shape1.png";
+import StarImg from "../../../assets/images/v1/star.png";
+import User1Img from "../../../assets/images/v1/user1.png";
+import User2Img from "../../../assets/images/v1/user2.png";
+import User3Img from "../../../assets/images/v1/user3.png";
+import { FadeInStaggerTwo, FadeInStaggerTwoChildren } from "../../animation/FadeInStaggerTwo";
+function HeroContent() {
+	return (
+		<div className="aximo-hero-content">
+			<FadeInStaggerTwo>
+				<FadeInStaggerTwoChildren>
+					<h1>
+						<span className="aximo-title-animation">
+						Here to generate
+							<img src={StarImg} alt="StarImg" />
+						</span>{" "}
+						Brand growth
+					</h1>
+				</FadeInStaggerTwoChildren>
+				<FadeInStaggerTwoChildren>
+					<p>
+						{`menjadi titik temu bagi kreativitas dan kapabilitas generasi muda dalam bidang creative & marketing untuk memenuhi kebutuhan pertumbuhan brand dan bisnis yang pada akhirnya memiliki tujuan mendukung perkembangan ekonomi yang sustainable bagi semua pihak.`}
+					</p>
+				</FadeInStaggerTwoChildren>
+				<FadeInStaggerTwoChildren>
+					<div className="aximo-hero-user-wrap">
+						<div className="aximo-hero-user-thumb">
+							<div className="aximo-hero-user-thumb-item">
+								<img src={User1Img} alt="User1Img" />
+							</div>
+							<div className="aximo-hero-user-thumb-item">
+								<img src={User3Img} alt="User3Img" />
+							</div>
+							<div className="aximo-hero-user-thumb-item">
+								<img src={User2Img} alt="User2Img" />
+							</div>
+						</div>
+						<div className="aximo-hero-user-data">
+							<p>Sudah dipercaya oleh berbagai Brand di Indonesia</p>
+						</div>
+					</div>
+				</FadeInStaggerTwoChildren>
+				<FadeInStaggerTwoChildren>
+					<Link className="aximo-call-btn" to="/contact-us">
+						Konsultasi GRATIS Sekarang<i className="icon-call"></i>
+					</Link>
+					<div className="aximo-hero-shape">
+						<img src={ShapeImg} alt="ShapeImg" />
+					</div>
+				</FadeInStaggerTwoChildren>
+			</FadeInStaggerTwo>
+		</div>
+	);
+}
+
+export default HeroContent;
